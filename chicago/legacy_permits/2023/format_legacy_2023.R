09403a2cde5fbbebb9d9762b4fce4c3970b138fb
Damonamajor <56321109+Damonamajor@users.noreply.github.com>
2025-12-18 09:47:03 -0600

    Update chicago/legacy_permits/2023/format_legacy_2023.R
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>

--- chicago/legacy_permits/2023/format_legacy_2023.R
+++ chicago/legacy_permits/2023/format_legacy_2023.R
@@ -45,7 +45,7 @@ need_worked <- read_xlsx_all_char(
     "Reinstated"          = Reinstated.permit,
     pin2, pin3, pin4, pin5, pin6, pin7, pin8, pin9, pin10
   ) %>%
-  mutate(`Applicant City, State, Zip* [ADDR3]` = "Chicago, IL",
+  mutate(`Applicant City, State, Zip* [ADDR3]` = "CHICAGO, IL",
   # if reinstated is not NA replace the value for Notes with it
   `Notes [NOTE1]` = ifelse(!is.na(`Reinstated`), `Reinstated`, `Notes [NOTE1]`)) %>%
   select(-c(`Reinstated`)) %>%
