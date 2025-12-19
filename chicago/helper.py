3707ddb58109dbd7a7de6fe03bce020748abfcec
Damonamajor <damon.major@cookcountyil.gov>
2025-12-19 16:19:27 +0000

    Add not-null check to valid_amount

--- chicago/helper.py
+++ chicago/helper.py
@@ -138,8 +138,8 @@ def finalize_columns(
     )
 
     df_flagged["valid_amount"] = (
-        pd.to_numeric(df_flagged["Amount* [AMOUNT]"], errors="coerce")
-        < 2147483647
+        pd.to_numeric(df_flagged["Amount* [AMOUNT]"], errors="coerce").notnull()
+        & (pd.to_numeric(df_flagged["Amount* [AMOUNT]"], errors="coerce") < 2147483647)
     )
 
     df_flagged["pin14_in_data"] = df_flagged["PIN* [PARID]"].isin(
