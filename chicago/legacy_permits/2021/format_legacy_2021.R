7e163d0be9a363168af226305961507924c7c854
Damonamajor <56321109+Damonamajor@users.noreply.github.com>
2026-02-17 11:05:03 -0600

    27 add formatting script for manually reviewed permits (#28)
    * initial push
    
    * csv files
    
    * use helper functions
    
    * Working review_permits_python_script
    
    * lintr and batches
    
    * Add gitignore
    
    * update to correct legacy scripts
    
    * renv
    
    * update xlsx files
    
    * other xlsx file
    
    * wrapup legacy permits
    
    * All four working scripts
    
    * modified helper
    
    * lintr
    
    * working py function
    
    * quick legacy fix
    
    * include xlsx upload
    
    * Add commenting
    
    * commenting
    
    * lintr
    
    * Delete format_legacy_2021.R
    
    * Delete .Rprofile
    
    * Delete renv/.gitignore
    
    * Delete renv/activate.R
    
    * Delete chicago/.gitignore
    
    * Delete chicago/legacy_permits/2021/2021 manual review processed- JW completed (1).xlsx
    
    * Delete chicago/renv.lock
    
    * renv
    
    * write to correct output folder
    
    * update gitignore
    
    * Delete chicago/legacy_permits/2021/2021 manual review processed.xlsx
    
    * Delete chicago/legacy_permits/2023/2023permits_processed_2.xlsx
    
    * Delete chicago/legacy_permits/2022/2022 City permits for manual review v5processed (005).xlsx
    
    * Delete chicago/Copy of 2024-01-01_to_2025-09-01_permits_needing_review12.8.25.xlsx
    
    * Update chicago/legacy_permits/2021/format_legacy_2021.R
    
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>
    
    * Update chicago/legacy_permits/2021/format_legacy_2021.R
    
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>
    
    * Update chicago/legacy_permits/helper.R
    
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>
    
    * Update chicago/legacy_permits/2022/format_legacy_2022.R
    
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>
    
    * Update chicago/legacy_permits/helper.R
    
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>
    
    * Update chicago/legacy_permits/helper.R
    
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>
    
    * Update chicago/legacy_permits/helper.R
    
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>
    
    * Update chicago/legacy_permits/2023/format_legacy_2023.R
    
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>
    
    * Update chicago/helper.py
    
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>
    
    * Update chicago/legacy_permits/2021/format_legacy_2021.R
    
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>
    
    * Delete chicago/renv/.gitignore
    
    * switch paths
    
    * update helper
    
    * Add in checking for valid pins
    
    * lintr
    
    * use MDY
    
    * update date
    
    * Update .gitignore
    
    * Update .gitignore
    
    * simplify xlsx upload
    
    * Add not-null check to valid_amount
    
    * Add not null check to amount in py helper
    
    * convert amount check to convert to numeric
    
    * verify_legacy_upload_script
    
    * Add crosswalk for legacy permits
    
    * changes based on first draft of final upload
    
    * lintr
    
    * qc changes
    
    * update to xlsx outputs
    
    * lintr
    
    * Rename to # [LLINE]
    
    * add de-duplication to all files
    
    * Add gitignore
    
    * gitignore
    
    * update changes-separate pin and other errors - add flags to output - include flags for qc
    
    * lintr
    
    * Update chicago/legacy_permits/2021/format_legacy_2021.R
    
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>
    
    * Update .gitignore
    
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>
    
    * Update .pre-commit-config.yaml
    
    * Create .lintr
    
    * lintr
    
    * move renv
    
    * lintr
    
    * lintr
    
    * lintr
    
    * lintr
    
    * update lintr
    
    * lintr
    
    * update lintr
    
    * add enter to lintr
    
    * test lintr
    
    * lintr
    
    * lintr
    
    * Update .lintr
    
    * lintr
    
    * lintr
    
    * lintr
    
    * lintr
    
    * lintr
    
    ---------
    
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>

---
+++ chicago/legacy_permits/2021/format_legacy_2021.R
@@ -0,0 +1,69 @@
+library(dplyr)
+library(openxlsx)
+library(tidyr)
+library(openxlsx)
+
+source("helper.R")
+
+crosswalk <- read.xlsx("crosswalk.xlsx") %>%
+  filter(year == "2021") %>%
+  select(meta_pin, original_pin) %>%
+  mutate(
+    meta_pin = as.character(meta_pin),
+    original_pin = as.character(original_pin)
+  )
+
+need_worked <- read_xlsx_all_char(
+  "2021/2021 manual review processed.xlsx",
+  sheet = "Need worked"
+) %>%
+  mutate(
+    `Applicant Street Address* [ADDR1]` =
+      paste(STREET_NUMBER, STREET.DIRECTION, STREET_NAME, SUFFIX)
+  ) %>%
+  select(
+    "PIN* [PARID]" = PIN1,
+    "Local Permit No.* [USER28]" = `PERMIT#`,
+    "Issue Date* [PERMDT]" = ISSUE_DATE,
+    "Amount* [AMOUNT]" = REPORTED_COST,
+    "Applicant Street Address* [ADDR1]",
+    "Applicant* [USER21]" = CONTACT_1_NAME,
+    "Notes [NOTE1]" = WORK_DESCRIPTION,
+    PIN2, PIN3, PIN4, PIN5, PIN6, PIN7
+  ) %>%
+  mutate(`Applicant City, State, Zip* [ADDR3]` = "CHICAGO, IL") %>%
+  expand_pins() %>%
+  ensure_columns(column_order) %>%
+  mutate(
+    `PIN* [PARID]` = normalize_pin(`PIN* [PARID]`),
+    `Issue Date* [PERMDT]` = format(
+      as.Date(as.numeric(`Issue Date* [PERMDT]`), origin = "1899-12-30"),
+      "%m/%d/%Y"
+    )
+  ) %>%
+  left_join(crosswalk, by = c("PIN* [PARID]" = "original_pin")) %>%
+  # Replace PIN* [PARID] with meta_pin from crosswalk only if it is not NA
+  mutate(`PIN* [PARID]` = coalesce((meta_pin), (`PIN* [PARID]`))) %>%
+  select(-meta_pin) %>%
+  group_by(
+    # This deduplicates PINs. The most common cuplrit for this is OHare which
+    # has many pins in the PIN or parid column, but a single pin in the PIN1
+    # column.
+    `PIN* [PARID]`,
+    `Local Permit No.* [USER28]`
+  ) %>%
+  slice(1) %>%
+  ungroup() %>%
+  finalize_columns(needed_columns)
+
+write.xlsx(
+  need_worked$upload,
+  "2021/2021permits_processed_legacy_need_worked_upload.xlsx",
+  rowNames = FALSE
+)
+
+write.xlsx(
+  need_worked$need_review,
+  "2021/2021permits_processed_legacy_need_worked_review.xlsx",
+  rowNames = FALSE
+)
---
+++ chicago/legacy_permits/2022/format_legacy_2022.R
@@ -0,0 +1,113 @@
+library(dplyr)
+library(openxlsx)
+library(tidyr)
+library(openxlsx)
+
+source("helper.R")
+
+crosswalk <- read.xlsx("crosswalk.xlsx") %>%
+  filter(year == "2022") %>%
+  select(meta_pin, original_pin) %>%
+  mutate(
+    meta_pin = as.character(meta_pin),
+    original_pin = as.character(original_pin)
+  )
+
+actionable <- read_xlsx_all_char(
+  "2022/2022 City permits for manual review v5processed (005).xlsx",
+  sheet = "Actionable"
+) %>%
+  select(
+    "PIN* [PARID]" = PIN1,
+    "Local Permit No.* [USER28]" = Local.Permit.No,
+    "Issue Date* [PERMDT]" = ISSUE_DATE,
+    "Amount* [AMOUNT]" = Amount,
+    "Applicant Street Address* [ADDR1]" = Street.Address,
+    "Applicant* [USER21]" = Applicant,
+    "Notes [NOTE1]" = Notes,
+    PIN2, PIN3, PIN4, PIN5, PIN6, PIN7
+  ) %>%
+  mutate(`Applicant City, State, Zip* [ADDR3]` = "CHICAGO, IL") %>%
+  expand_pins() %>%
+  ensure_columns(column_order) %>%
+  mutate(
+    `PIN* [PARID]` = normalize_pin(`PIN* [PARID]`),
+    `Issue Date* [PERMDT]` = format(
+      as.Date(as.numeric(`Issue Date* [PERMDT]`), origin = "1899-12-30"),
+      "%m/%d/%Y"
+    )
+  ) %>%
+  left_join(crosswalk, by = c("PIN* [PARID]" = "original_pin")) %>%
+  # Replace PIN* [PARID] with meta_pin from crosswalk only if it is not NA
+  mutate(`PIN* [PARID]` = coalesce((meta_pin), (`PIN* [PARID]`))) %>%
+  # Remove a pin which is valid 14 digits but all 0's
+  filter(`PIN* [PARID]` != "00000000000000") %>%
+  select(-meta_pin) %>%
+  # There are duplicate pins in this script
+  group_by(
+    `PIN* [PARID]`,
+    `Local Permit No.* [USER28]`
+  ) %>%
+  slice(1) %>%
+  ungroup() %>%
+  finalize_columns(needed_columns)
+
+need_worked <- read_xlsx_all_char(
+  "2022/2022 City permits for manual review v5processed (005).xlsx",
+  sheet = "Need worked"
+) %>%
+  select(
+    "PIN* [PARID]" = PIN1,
+    "Local Permit No.* [USER28]" = Local.Permit.No,
+    "Issue Date* [PERMDT]" = ISSUE_DATE,
+    "Amount* [AMOUNT]" = Amount,
+    "Applicant Street Address* [ADDR1]" = Street.Address,
+    "Applicant* [USER21]" = Applicant,
+    "Notes [NOTE1]" = Notes,
+    PIN2, PIN3, PIN4, PIN5, PIN6, PIN7
+  ) %>%
+  mutate(`Applicant City, State, Zip* [ADDR3]` = "Chicago, IL") %>%
+  expand_pins() %>%
+  ensure_columns(column_order) %>%
+  mutate(
+    `PIN* [PARID]` = normalize_pin(`PIN* [PARID]`),
+    `Issue Date* [PERMDT]` = format(
+      as.Date(as.numeric(`Issue Date* [PERMDT]`), origin = "1899-12-30"),
+      "%m/%d/%Y"
+    )
+  ) %>%
+  left_join(crosswalk, by = c("PIN* [PARID]" = "original_pin")) %>%
+  # Replace PIN* [PARID] with meta_pin from crosswalk only if it is not NA
+  mutate(`PIN* [PARID]` = coalesce((meta_pin), (`PIN* [PARID]`))) %>%
+  select(-meta_pin) %>%
+  group_by(
+    `PIN* [PARID]`,
+    `Local Permit No.* [USER28]`
+  ) %>%
+  slice(1) %>%
+  ungroup() %>%
+  finalize_columns(needed_columns)
+
+openxlsx::write.xlsx(
+  need_worked$upload,
+  "2022/2022permits_processed_legacy_need_worked_upload.xlsx",
+  rowNames = FALSE
+)
+
+openxlsx::write.xlsx(
+  need_worked$need_review,
+  "2022/2022permits_processed_legacy_need_worked_review.xlsx",
+  rowNames = FALSE
+)
+
+openxlsx::write.xlsx(
+  actionable$upload,
+  "2022/2022permits_processed_legacy_actionable_upload.xlsx",
+  rowNames = FALSE
+)
+
+openxlsx::write.xlsx(
+  actionable$need_review,
+  "2022/2022permits_processed_legacy_actionable_review.xlsx",
+  rowNames = FALSE
+)
