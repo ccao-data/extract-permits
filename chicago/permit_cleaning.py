870fc69705eac232e2dd174c8e5b0828c1eb2b11
Damonamajor <56321109+Damonamajor@users.noreply.github.com>
2026-03-13 14:26:54 -0500

    Update permit_cleaning.py

--- chicago/permit_cleaning.py
+++ chicago/permit_cleaning.py
@@ -67,6 +67,7 @@ def parse_args() -> tuple[str, str, bool]:
         sys.exit(1)
 
     deduplicate = deduplicate.lower() == "true"
+    
     return start_date_str, end_date_str, deduplicate
 
 
@@ -111,6 +112,7 @@ def pull_existing_pins_from_athena(cursor: Cursor, year: str) -> pd.DataFrame:
     chicago_pin_universe = as_pandas(cursor)
     pin_cache_filename = get_pin_cache_filename(year)
     chicago_pin_universe.to_csv(pin_cache_filename, index=False)
+    
     return chicago_pin_universe
 
 
